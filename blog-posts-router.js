9ff656c078aee9557775c6a11fa1d1c5ed508a17
Michelle Manzogit config --global user.email memanzo17@gmail.com <memanzo17@gmail.com>
2018-03-25 23:19:39 -0400

    added GET, POST, PUT, DELETE functionality

---
+++ blog-posts-router.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const router = express.Router()
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json()
+
+const {BlogPosts} = require('./models')
+
+BlogPosts.create('Learning Node', 'Learning node is an excellent adventure', 'Michelle Manzo')
+
+router.get('/', (req, res) => {
+  res.json(BlogPosts.get())
+})
+
+
+router.post('/', jsonParser, (req, res) => {
+	const requiredFields = ['title', 'content', 'author']
+	for (let i=0; i<requiredFields.length; i++) {
+	    const field = requiredFields[i]
+	    if (!(field in req.body)) {
+	      const message = `Missing \`${field}\` in request body`
+	      console.error(message);
+	      return res.status(400).send(message)
+    }
+  }
+  const post = BlogPosts.create(req.body.title, req.body.content, req.body.author)
+  res.status(201).json(post)
+})
+
+
+router.delete('/:id', (req, res) => {
+	if (req.params.id !== req.body.id) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id `
+      `(${req.body.id}) must match`)
+    console.error(message)
+    return res.status(400).send(message)
+  }
+	BlogPosts.delete(req.params.id)
+	console.log(`Deleted blog post \`${req.params.ID}\``)
+  	res.status(204).end()
+})
+
+router.put('/:id', jsonParser, (req, res) => {
+	const requiredFields = ['title', 'content', 'author']
+	for (let i=0; i<requiredFields.length; i++) {
+	    const field = requiredFields[i]
+	    if (!(field in req.body)) {
+	      const message = `Missing \`${field}\` in request body`
+	      console.error(message);
+	      return res.status(400).send(message)
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id `
+      `(${req.body.id}) must match`)
+    console.error(message)
+    return res.status(400).send(message)
+  }
+  console.log(`Updating blog post \`${req.params.id}\``)
+  const updatedPost = BlogPosts.update({
+    id: req.params.id,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,	
+    publishData: req.body.PublishDate
+  })
+  res.status(204).end()
+})
+
+
+module.exports = router
